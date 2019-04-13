985bd54c3455c80deebe6308019fdfa358d258a0
Parul <parulmc@gmail.com>
2019-04-12 23:36:16 -0400

    Objects, properties, JSON

---
+++ myMusic_JSON_array.js
@@ -0,0 +1,25 @@
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [ 
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  // Add record here
+  {
+    "artist": "Adele",
+    "title": "Fire",
+    "release_year": 2005,
+    "formats": [
+      "CD",
+      "4T",
+      "TO"
+    ],
+    "gold": false
+  }
+];
